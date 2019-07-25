7f4799553770c7567a4b930c0be660e5b4fd33d6
xuedao <34347476@qq.com>
2019-07-25 12:03:31 +0800

    侧边导航栏完成，未做跳转

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,28 +1,92 @@
 <template>
   <el-container>
-    <el-header>Header
-      <el-button type="infor"
+    <el-header>
+      <div class="title">
+        <img src="../assets/heima.png"
+             alt="heima">
+        <p>黑马后台管理系统</p>
+      </div>
+      <el-button type="primary"
                  class="log-out"
                  @click="logOut">退出</el-button>
     </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
-      <el-main>Main</el-main>
+      <el-aside :width="isOpen ? '64px' : '200px'">
+        <div class="toggle-button"
+             @click="isOpen = !isOpen">|||
+        </div>
+
+        <el-menu :unique-opened="true"
+                 default-active="2"
+                 class="el-menu-vertical-demo"
+                 background-color="#545c64"
+                 text-color="#fff"
+                 active-text-color="#ffd04b"
+                 :collapse-transition="false"
+                 :collapse="isOpen">
+          <!-- 一级菜单 -->
+          <el-submenu v-for="item in menuList"
+                      :key="item.id"
+                      :index="item.id + ''">
+            <template slot="title">
+              <i :class="iconObj[item.id]"></i>
+              <span v-cloak>{{ item.authName }}</span>
+            </template>
+            <el-menu-item v-for="subItem in item.children"
+                          :key="subItem.id"
+                          :index="subItem.id + ''">
+              <template slot="title">
+                <i :class="iconObj[subItem.id]"></i>
+                <span v-cloak>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 
 </template>
-
 <script>
 export default {
   data() {
-    return {}
+    return {
+      menuList: [],
+      iconObj: {
+        '125': 'el-icon-user-solid',
+        '103': 'el-icon-s-tools',
+        '101': 'el-icon-s-goods',
+        '102': 'el-icon-s-order',
+        '145': 'el-icon-s-data',
+        '110': 'el-icon-user',
+        '111': 'el-icon-s-custom',
+        '112': 'el-icon-s-custom',
+        '104': 'el-icon-coordinate',
+        '115': 'el-icon-coordinate',
+        '121': 'el-icon-coordinate',
+        '107': 'el-icon-coordinate',
+        '146': 'el-icon-coordinate'
+      },
+      isOpen: false
+    }
   },
   methods: {
     logOut() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('/menus')
+      console.log(res)
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
     }
+  },
+  created() {
+    this.getMenuList()
   }
 }
 </script>
@@ -31,15 +95,38 @@ export default {
 .el-container {
   height: 100%;
   .el-header {
-    background-color: #2b4b6b;
+    background-color: #545c64;
+    .title {
+      display: flex;
+      align-items: center;
+
+      p {
+        font-size: 18px;
+        color: floralwhite;
+        margin-left: 30px;
+      }
+    }
     .log-out {
       position: absolute;
       top: 10px;
       right: 20px;
+      font-size: 14px;
     }
   }
   .el-aside {
-    background-color: pink;
+    background-color: #545c64;
+    .el-menu {
+      border-right: none;
+    }
   }
+}
+.toggle-button {
+  background: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,10 +8,19 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求的基准URL地址
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios设置请求拦截器
+axios.interceptors.request.use(
+  config => {
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
+  },
+  err => {
+    console.log(err)
+  }
+)
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
-
 
 new Vue({
   router,
